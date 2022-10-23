01e8e9c87ead9ede88d68d577e247b228d9d08d5
chan_we <chan_we@foxmail.com>
2022-10-23 21:08:49 +0800

    feat: detail answer

--- src/components/CQuestion/index.tsx
+++ src/components/CQuestion/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./index.less";
 import { useBoolean } from "ahooks";
 import {
+  UpOutlined,
   DownOutlined,
   CaretUpOutlined,
   CaretDownOutlined,
@@ -41,37 +42,67 @@ const tailItems = [
 ];
 
 function CQuestion(props: IProps) {
-  const [isAll] = useBoolean(false);
+  const [isAll, isAllActions] = useBoolean(false);
   const { target } = props.data;
+  const { author } = target;
+
   return (
     <div className="c-question">
       <div className="c-question-title">{target.question.title}</div>
       <div className="c-question-content">
-        {!isAll && (
+        {isAll ? (
           <div>
-            {target.author.name}：{target.excerpt_new}
-            <a>
+            <div className="c-question-content-author">
+              <span className="c-question-content-author-avatar">
+                <img src={author.avatar_url} alt="" />
+              </span>
+              <span className="c-question-content-author-name">
+                {author.name}
+              </span>
+              <span className="c-question-content-author-headline">
+                {author.headline}
+              </span>
+            </div>
+            <p className="c-question-content-voteup">
+              {(target.voteup_count as number).toLocaleString()}人赞同了该答案
+            </p>
+            <div dangerouslySetInnerHTML={{ __html: target.content }}></div>
+          </div>
+        ) : (
+          <div>
+            {author.name}：{target.excerpt_new}
+            <a onClick={isAllActions.setTrue}>
               阅读全文 <DownOutlined />
             </a>
           </div>
         )}
       </div>
       <div className="c-question-tail">
-        <div>
-          <Button>
-            <CaretUpOutlined />
-            赞同
-          </Button>
-          <Button>
-            <CaretDownOutlined />
-          </Button>
+        <div className="c-question-tail-left">
+          <div>
+            <Button>
+              <CaretUpOutlined />
+              赞同
+            </Button>
+            <Button>
+              <CaretDownOutlined />
+            </Button>
+          </div>
+          {tailItems.map((item, i) => (
+            <span key={i}>
+              {item.icon}
+              {item.label}
+            </span>
+          ))}
         </div>
-        {tailItems.map((item, i) => (
-          <span key={i}>
-            {item.icon}
-            {item.label}
-          </span>
-        ))}
+        <div className="c-question-tail-right">
+          {isAll && (
+            <span onClick={isAllActions.setFalse}>
+              收起
+              <UpOutlined />
+            </span>
+          )}
+        </div>
       </div>
     </div>
   );
