6545a529799b3553a54ffcbdaebd6b0645700d98
chenliangwei <chenliangwei@bilibili.com>
2022-10-24 14:29:19 +0800

    feat: 滚动加载

--- src/pages/RecommandPage/index.tsx
+++ src/pages/RecommandPage/index.tsx
@@ -1,25 +1,46 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { CQuestion } from "../../components";
-import { Skeleton } from "antd";
+import { List, Skeleton } from "antd";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function RecommandPage() {
   const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState([]);
 
   useEffect(() => {
+    appendData();
+  }, []);
+
+  const appendData = () => {
     setLoading(true);
     axios.get("/api/content").then((res: any) => {
-      setQuestions(res.data);
+      setQuestions(questions.concat(res.data));
       setLoading(false);
-      console.log(res.data);
     });
-  }, []);
+  };
 
   return (
     <div className="recommand-page">
       {loading &&
-        Array(6).fill(
+        Array(6)
+          .fill("")
+          .map((_, i) => (
+            <Skeleton
+              key={i}
+              active
+              style={{
+                backgroundColor: "white",
+                borderBottom: "1px solid #f0f2f7",
+                padding: "20px",
+              }}
+            />
+          ))}
+      <InfiniteScroll
+        dataLength={questions.length}
+        next={appendData}
+        hasMore={true}
+        loader={
           <Skeleton
             active
             style={{
@@ -28,10 +49,18 @@ function RecommandPage() {
               padding: "20px",
             }}
           />
-        )}
-      {questions.map((question: any) => (
-        <CQuestion data={question} key={question.id} />
-      ))}
+        }
+      >
+        <List
+          dataSource={questions}
+          renderItem={(question: any) => (
+            <CQuestion data={question} key={question.id + Math.random()} />
+          )}
+        />
+      </InfiniteScroll>
+      {/* {questions.map((question: any) => (
+        <CQuestion data={question} key={question.id + Math.random()} />
+      ))} */}
     </div>
   );
 }
